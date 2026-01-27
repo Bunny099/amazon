7dadce3e1e7d01e0be7a5e3fda6faa82057d834b
Bunny099 <jayeshkhuman121@gmail.com>
2026-01-27 22:48:02 +0530

    server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("hello")
+import app from "./app.js"
+const PORT = process.env.PORT;
+if(!PORT){
+    throw new Error("Port not found!")
+}
+app.listen(PORT,()=>{console.log(`Server is running on PORT: ${PORT}`)})

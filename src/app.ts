55db781fae564f74b2f49af3605a1eb1a29dfcf3
Bunny099 <jayeshkhuman121@gmail.com>
2026-01-29 21:24:21 +0530

    app router

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express  from "express";
+import cors from "cors"
+import "dotenv/config"
+import { authRouter } from "./routes/auth.route.js";
+const app = express();
+
+app.use(express.json())
+app.use(cors());
+
+app.use("/auth",authRouter)
+export default app;

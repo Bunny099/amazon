7c40d883005aca117164621f41366fcd8d8f57d0
Bunny099 <jayeshkhuman121@gmail.com>
2026-02-04 22:59:58 +0530

    feat:seller route for product

--- src/routes/seller.route.ts
+++ src/routes/seller.route.ts
@@ -1,3 +1,6 @@
 import { Router } from "express";
+import { middleware } from "../middleware/middleware.js";
+import { sellerController } from "../controllers/seller.controller.js";
 
 export const sellerRourer = Router()
+sellerRourer.post("/product",middleware,sellerController)

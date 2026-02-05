488d15ac984490f3144385e0e83619fc5e66a47a
Bunny099 <jayeshkhuman121@gmail.com>
2026-02-05 22:51:27 +0530

    feat:(sellercontroller) all product controllers

--- src/controllers/seller.controller.ts
+++ src/controllers/seller.controller.ts
@@ -1,8 +1,23 @@
 import { type Request,type Response } from "express"
-import { createProduct } from "../services/seller.service.js";
-export const sellerController = async(req:Request,res:Response)=>{
+import { createProduct,fetchProduct,patchProuct, deleteProduct } from "../services/seller.service.js";
+
+
+
+export const getSellerProductController = async(req:Request,res:Response)=>{
     try{
-        const {name} = req.body;
+        const user = req.user;
+        if(!user){
+            return res.status(401).json({message:"Unauthorized!"})
+        }
+        const response = await fetchProduct(user);
+        return res.status(200).json({response,message:"Products found!"})
+    }catch(e:any){
+        return res.status(500).json({message:e.message || "Server error!"})
+    }
+}
+export const sellerProductController = async(req:Request,res:Response)=>{
+    try{
+        const {name,price} = req.body;
         const user = req.user;
         if(!user){
             return res.status(401).json({message:"Unauthorized!"})
@@ -10,9 +25,44 @@ export const sellerController = async(req:Request,res:Response)=>{
         if(!name){
             return res.status(400).json({message:"Field missing!"})
         }
-        const response = await createProduct({user,name});
+        const response = await createProduct({user,name,price});
         return res.status(201).json({response,nessage:"Product created!"})
        
+    }catch(e:any){
+        return res.status(500).json({message:e.message || "Server error!"})
+    }
+}
+
+export const sellerPatchController = async(req:Request,res:Response)=>{
+    try{
+        const {productId,price} = req.params;
+        const user = req.user;
+        if(!user){
+            return res.status(401).json({message:"Unauthorized!"})
+        }
+        if(!productId || !price){
+            return res.status(400).json({message:"Field missing!"})
+        }
+        const response = await patchProuct({user,productId,price})
+        return res.status(200).json({response,message:"Product updated!"})
+
+    }catch(e:any){
+        return res.status(500).json({message:e.message || "Sever error!"})
+    }
+}
+
+export const deleteProductController = async(req:Request,res:Response)=>{
+    try{
+        const {productId} = req.params;
+        const user = req.user;
+        if(!user){
+            return res.status(401).json({message:"Unauthorized!"})
+        }
+        if(!productId){
+            return res.status(400).json({message:"Field missing!"})
+        }
+        const response = await deleteProduct({productId,user})
+        return res.status(200).json({response,message:"Product deleted!"})
     }catch(e:any){
         return res.status(500).json({message:e.message || "Server error!"})
     }

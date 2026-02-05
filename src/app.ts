103c8ed4bb79df19dd80215f72ce97c17e652735
Bunny099 <jayeshkhuman121@gmail.com>
2026-02-05 22:52:59 +0530

    chores:app file

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,8 @@ import express  from "express";
 import cors from "cors"
 import { authRouter } from "./routes/auth.route.js";
 import { customerRouter } from "./routes/customer.route.js";
-import { sellerRourer } from "./routes/seller.route.js";
+import { sellerRouter } from "./routes/seller.route.js";
 import { adminRouter } from "./routes/admin.route.js";
-import { middleware } from "./middleware/middleware.js";
 const app = express();
 
 app.use(express.json())
@@ -12,6 +11,6 @@ app.use(cors());
 
 app.use("/auth",authRouter)
 app.use("/customer",customerRouter)
-app.use("/seller",sellerRourer)
+app.use("/seller",sellerRouter)
 app.use("/admin",adminRouter)
 export default app;

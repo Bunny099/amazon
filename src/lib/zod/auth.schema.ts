79f9d002f8a2cde78540c2eb03ac228457060d9a
Bunny099 <jayeshkhuman121@gmail.com>
2026-02-06 21:47:02 +0530

    update:zod auth schema updated

--- src/lib/zod/auth.schema.ts
+++ src/lib/zod/auth.schema.ts
@@ -12,6 +12,12 @@ export const UserLoginSchema = z.object({
     password:z.string().min(6).max(80),
     role:RoleEnum.default("Customer")
 })
+
+export const UserSchema = z.object({
+    id:z.string(),
+    role:RoleEnum
+})
 export type CreateUserInput = z.infer<typeof CreateUserSchema>  
 export type LoginUserInput = z.infer<typeof UserLoginSchema>
 export type Role = z.infer<typeof RoleEnum>
+export type User = z.infer<typeof UserSchema>

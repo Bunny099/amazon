36950525cc9ddb2413cb3dc3b468d730850bba6f
Bunny099 <jayeshkhuman121@gmail.com>
2026-02-06 21:46:23 +0530

    feat:(seller service) zod types added

--- src/services/seller.service.ts
+++ src/services/seller.service.ts
@@ -1,59 +1,61 @@
 import { db } from "../lib/db.js";
 import type { Role } from "../lib/zod/auth.schema.js"
-type User ={id:string,role:Role}
-interface ProductData{
-    name:string,
-    price:string,
-    user:User
-}
-interface PatchProductData{
-    productId:string | string[],
-    price?:string | string[],
-    user:User
-}
+import type { ProductInputData } from "../lib/zod/seller.product.js";
+import type { User } from "../lib/zod/auth.schema.js";
+import type { ProductPatchData } from "../lib/zod/seller.product.js";
+import type { ProductDeleteData } from "../lib/zod/seller.product.js";
 
-export const fetchProduct = async(data:User)=>{
+
+export const fetchProduct = async (data: User) => {
     const user = data;
-    if(user.role !== "Seller"){
+    if (user.role !== "Seller") {
         throw new Error("Not authorized!")
     }
-    const response = await db.product.findMany();
-    if(!response){
+    const response = await db.product.findMany({ where: { sellerId: user.id } });
+    if (!response) {
         throw new Error("Products not found!")
     }
     return response;
 }
-export const createProduct = async(data:ProductData)=>{
-    const {name,price,user} = data;
-    if(user.role !=="Seller"){
+export const createProduct = async (data: ProductInputData) => {
+    const { name, price, user } = data;
+    if (user.role !== "Seller") {
         throw new Error("Not authorized!")
     }
-    const response = await db.product.create({data:{name,price}});
-    if(!response){
+    const response = await db.product.create({ data: { name, price, sellerId: user.id } });
+    if (!response) {
         throw new Error("Product not created!")
     }
     return response;
 }
-export const patchProuct = async(data:PatchProductData)=>{
-    const {user,productId,price} = data;
-    if(user.role  !== "Seller"){
+export const patchProuct = async (data: ProductPatchData) => {
+    const { user, productId, price } = data;
+    if (user.role !== "Seller") {
         throw new Error("Not authorized!")
     }
-   const response = await db.product.update({where:{id:productId as string},data:{price:price as string}});
-   if(!response){
-    throw new Error("Failed to update product!")
-   }
-   return response;
-    
+    const isProductExist = await db.product.findFirst({where:{id:productId,sellerId:user.id}});
+    if(!isProductExist){
+        throw new Error("Product not found!")
+    }
+    const response = await db.product.update({ where: { id: productId, sellerId: user.id }, data: { price } });
+    if (!response) {
+        throw new Error("Failed to update product!")
+    }
+    return response;
+
 }
 
-export const deleteProduct = async(data:PatchProductData)=>{
-    const {user,productId} = data;
-    if(user.role !== "Seller"){
+export const deleteProduct = async (data: ProductDeleteData) => {
+    const { user, productId } = data;
+    if (user.role !== "Seller") {
         throw new Error("Not authorized!")
     }
-    const response = await db.product.delete({where:{id:productId as string}});
-    if(!response){
+    const isProductExist = await db.product.findFirst({where:{id:productId,sellerId:user.id}});
+    if(!isProductExist){
+        throw new Error("Product not found!")
+    }
+    const response = await db.product.delete({ where: { id: productId } });
+    if (!response) {
         throw new Error("Product not deleted!")
     }
     return response

0158d8945c317f11a4aa5ce1c2dc6760d6e57951
Bunny099 <jayeshkhuman121@gmail.com>
2026-02-07 21:47:21 +0530

    feat:(seller inventory route)

--- src/routes/seller.route.ts
+++ src/routes/seller.route.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { middleware } from "../middleware/middleware.js";
-import { sellerProductController ,sellerPatchController,getSellerProductController,deleteProductController} from "../controllers/seller.controller.js";
+import { sellerProductController ,sellerPatchController,getSellerProductController,deleteProductController , getSellerInventory,createInventoryController} from "../controllers/seller.controller.js";
 
 export const sellerRouter = Router()
-sellerRouter.get("/product",middleware,getSellerProductController)
-sellerRouter.post("/product",middleware,sellerProductController)
-sellerRouter.patch("/product/:productId",middleware,sellerPatchController)
-sellerRouter.delete("/product/:productId",middleware,deleteProductController)
+//seller products
+sellerRouter.get("/product",middleware,getSellerProductController);
+sellerRouter.post("/product",middleware,sellerProductController);
+sellerRouter.patch("/product/:productId",middleware,sellerPatchController);
+sellerRouter.delete("/product/:productId",middleware,deleteProductController);
+
+//Inventories
+sellerRouter.get("/inventory/:productId",middleware,getSellerInventory);
+sellerRouter.post("/inventory",middleware,createInventoryController);
+

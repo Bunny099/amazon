4bca8c9e9bae2735c8fe26e666fabeaf2e3d2cfb
Bunny099 <jayeshkhuman121@gmail.com>
2026-02-12 09:11:24 +0530

    customer cart route

--- src/routes/customer.route.ts
+++ src/routes/customer.route.ts
@@ -1,6 +1,15 @@
 import { Router } from "express";
 import { middleware } from "../middleware/middleware.js";
+import { createCartCustomerController,cancleOrderCustomerController,createOrderCustomerController } from "../controllers/customer.controller.js";
 
 export const customerRouter = Router();
 
-customerRouter.post("/",middleware,)
+
+//customer cart and cartItem
+customerRouter.post("/cart",middleware,createCartCustomerController)
+
+//customer order
+customerRouter.post("/order",middleware,createOrderCustomerController)
+
+//customer canclation-return
+customerRouter.post("/order/cancle",middleware,cancleOrderCustomerController)

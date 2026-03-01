d3583b472680c8a0102ffe02a9dc4127e33d383b
Bunny099 <jayeshkhuman121@gmail.com>
2026-03-01 21:59:28 +0530

    feat:new patch & delete routes added

--- src/routes/customer.route.ts
+++ src/routes/customer.route.ts
@@ -1,15 +1,22 @@
 import { Router } from "express";
 import { middleware } from "../middleware/middleware.js";
-import { createCartCustomerController,cancleOrderCustomerController,createOrderCustomerController } from "../controllers/customer.controller.js";
+import { createCartCustomerController,cancleOrderCustomerController,createOrderCustomerController,getCartCustomerController,editCartCustomerController,deleteCartCustomerController } from "../controllers/customer.controller.js";
 
 export const customerRouter = Router();
 
 
 //customer cart and cartItem
-customerRouter.post("/cart",middleware,createCartCustomerController)
+customerRouter.post("/cart",middleware,createCartCustomerController);
+customerRouter.get("/cart",middleware,getCartCustomerController);
+customerRouter.patch("/cart/:productId",middleware,editCartCustomerController);
+customerRouter.delete("/cart/:productId",middleware,deleteCartCustomerController)
+
+//customer checkout
+
 
 //customer order
 customerRouter.post("/order",middleware,createOrderCustomerController)
+
 
 //customer canclation-return
 customerRouter.post("/order/cancle",middleware,cancleOrderCustomerController)

0199a68cb7cfb9537c5f971331b3a3ded75c96ca
Bunny099 <jayeshkhuman121@gmail.com>
2026-03-03 19:44:25 +0530

    feat:checkout and order cancel customer controllers

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -1,6 +1,6 @@
 import { type Request, type Response } from "express"
-import { createCart, createOrder, getCart, editCart, deleteCart, cancleOrder } from "../services/custonmer.service.js";
-import { CartDeleteSchema, CartSchema, OrderSchema } from "../lib/zod/customer.schema.js"
+import { createCart, createOrder, getCart, editCart, deleteCart, cancelOrder } from "../services/customer.service.js";
+import { CartDeleteSchema, CartSchema, OrderCancelSchema, OrderSchema } from "../lib/zod/customer.schema.js"
 import { UserSchema } from "../lib/zod/auth.schema.js";
 
 
@@ -22,33 +22,47 @@ export const createCartCustomerController = async (req: Request, res: Response) 
 }
 
 export const getCartCustomerController = async (req: Request, res: Response) => {
-    const input = { user: req.user };
-    const parse = UserSchema.safeParse(input);
-    if (!parse.success) {
-        throw new Error("Invalid field!")
+    try {
+        const user = req.user;;
+        if (!user) {
+            throw new Error("Invalid Field!")
+        }
+        const response = await getCart(user);
+        return res.status(200).json({ response, message: "Cart fetch successfully!" })
+    } catch (e: any) {
+        return res.status(500).json({ message: e.message || "Server Error!" })
     }
-    const response = await getCart(parse.data);
-    return res.status(200).json({ response, message: "Cart fetch successfully!" })
+
 };
 
 export const editCartCustomerController = async (req: Request, res: Response) => {
-    const input = { ...req.body, ...req.params, user: req.user };
-    const parse = CartSchema.safeParse(input);
-    if (!parse.success) {
-        throw new Error("Invalid fields!")
-    }
-    const response = await editCart(parse.data)
-    return res.status(200).json({ response, message: "Cart item updated!" })
-};
-export const deleteCartCustomerController = async (req: Request, res: Response) => {
-    const input = { ...req.body, user: req.user };
-    const parse = CartDeleteSchema.safeParse(input)
-    if (!parse.success) {
-        throw new Error("Invalid fields!")
+    try {
+        const input = { ...req.body, ...req.params, user: req.user };
+        const parse = CartSchema.safeParse(input);
+        if (!parse.success) {
+            throw new Error("Invalid fields!")
+        }
+        const response = await editCart(parse.data)
+        return res.status(200).json({ response, message: "Cart item updated!" })
+    } catch (e: any) {
+        return res.status(500).json({ message: e.message || "Server Error!" })
     }
 
-    const resposne = await deleteCart(parse.data);
-    return res.status(200).json({ message: "Cart item deleted!" })
+};
+export const deleteCartCustomerController = async (req: Request, res: Response) => {
+    try {
+        const input = { ...req.body, user: req.user };
+        const parse = CartDeleteSchema.safeParse(input)
+        if (!parse.success) {
+            throw new Error("Invalid fields!")
+        }
+
+        const resposne = await deleteCart(parse.data);
+        return res.status(200).json({ message: "Cart item deleted!" })
+    } catch (e: any) {
+        return res.status(500).json({ message: e.message || "Server Error!" })
+    }
+
 };
 
 export const createOrderCustomerController = async (req: Request, res: Response) => {
@@ -65,9 +79,15 @@ export const createOrderCustomerController = async (req: Request, res: Response)
     }
 }
 
-export const cancleOrderCustomerController = async (req: Request, res: Response) => {
+export const cancelOrderCustomerController = async (req: Request, res: Response) => {
     try {
-       
+        const input = { ...req.params, user: req.user };
+        const parse = OrderCancelSchema.safeParse(input);
+        if (!parse.success) {
+            throw new Error("Invalid fields");
+        }
+        const response = await cancelOrder(parse.data);
+        return res.status(200).json({ response, message: "Order cancel" })
     } catch (e: any) {
         return res.status(500).json({ message: e.message || "Server error!" })
     }
